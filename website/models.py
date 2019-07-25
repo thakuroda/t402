4e90d262b70d37600e6f6da9dce2b8859521585f
thakuroda <engkuroda@gmail.com>
2019-07-25 16:58:22 -0300

    criar models, formulários e salvar

--- website/models.py
+++ website/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    GENEROS = (
+        ('M' , 'masculino'),
+        ('F','feminino'),
+        ('O', 'outros')
+        )
+
+
+    nome = models.CharField(
+        max_length=255,
+        verbose_name='Nome'
+    )
+    sobrenome = models.CharField(
+        max_length=255,
+        verbose_name='Sobrenome'
+    )
+    genero = models.CharField(
+        max_length=255,
+        verbose_name='Gênero',
+        choices=GENEROS
+    )
+    email = models.EmailField(
+        max_length=255,
+        verbose_name='Email',
+        blank=False
+    )
+    data_de_criacao = models.DateTimeField(auto_now_add=True)
+    ativo = models.BooleanField(default=True)
--- website/views.py
+++ website/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
+from website.models import Pessoa
 
 # Create your views here.
 
 def index(request):
-    contexto = {
+    contexto={}
+    if request.method == 'POST':
+        pessoa = Pessoa()
+        pessoa.nome = request.POST.get('nome')
+        pessoa.sobrenome = request.POST.get ('sobrenome')
+        pessoa.email = request.POST.get ('email')
+        pessoa.genero = request.POST.get ('genero')
+        print(pessoa)
+        pessoa.save()
+        print('opaaaa tá salvando')
+        contexto =  {'msg': 'Parabéns :)'}
 
-    }
     return render(request, 'index.html', contexto)
 
 
 def sobre(request):
+    pessoa = Pessoa.objects
     contexto = {
         
     }
